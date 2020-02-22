9596266bb54981ec126b231209564104e92121f9
masterofsync <bikzzzzz@gmail.com>
2020-02-22 00:21:54 -0800

    Added .NET Core Web application project for RESTful APIs. Working on User Authentication.

---
+++ TournamentWebApiSolution/TournamentWebApi/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TournamentWebApi.Models;
+
+namespace TournamentWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            this._roleManager = roleManager;
+            this._userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            //string[] roles = { "Admin" };
+
+            //var roleExist = await _roleManager.RoleExistsAsync(roles[0]);
+
+            //if (roleExist == false)
+            //{
+            //    await _roleManager.CreateAsync(new IdentityRole(roles[0]));
+            //}
+
+            //var user = await _userManager.FindByEmailAsync("bikzzzzz@gmail.com");
+
+            //if (user !=null)
+            //{
+            //    await _userManager.AddToRoleAsync(user, "Admin");
+            //}
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TournamentWebApiSolution/TournamentWebApi/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TournamentWebApi.Data;
+using TournamentWebApi.Models;
+
+namespace TournamentWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            this._userManager = userManager;
+        }
+
+        //[HttpGet]
+        //public UserModel GetById()
+        //{
+        //    string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //    UserData data = new UserData();
+
+        //    return data.GetUserById(userId).First(); 
+        //}
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/User/Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> userList = new List<ApplicationUserModel>();
+
+            var users = _context.Users.ToList();
+            var uRoles = from userRoles in _context.UserRoles
+                        join r in _context.Roles on userRoles.RoleId equals r.Id
+                        select new { userRoles.UserId, userRoles.RoleId, r.Name };
+
+
+            foreach (var user in users)
+            {
+                ApplicationUserModel userModel = new ApplicationUserModel
+                {
+                    Id = user.Id,
+                    Email = user.Email
+                };
+
+                userModel.Roles = uRoles.Where(x => x.UserId == userModel.Id)
+                                        .ToDictionary(key =>key.RoleId,val=>val.Name);
+
+                userList.Add(userModel);
+            }
+            return userList;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/User/Admin/GetAllRoles")]
+        public Dictionary<string,string> GetAllRoles()
+        {
+            var roles = _context.Roles.ToDictionary(x => x.Id, x => x.Name);
+            return roles;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/AddRole")]
+        public async Task AddRole(UserRolePairModel pairing)
+        {
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.AddToRoleAsync(user, pairing.RoleName);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/RemoveRole")]
+        public async Task RemoveARole(UserRolePairModel pairing)
+        {
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.RemoveFromRoleAsync(user, pairing.RoleName);
+        }
+    }
+}
